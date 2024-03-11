45bed234d25cb70553517326fa3e7b03693de21b
Илья Олегович Юрьев <i.o.yurev@yandex.ru>
2024-03-11 20:02:47 +0500

    fix(build.cmd): исправлена невозможность сборки из-за отсутствия попдпапки для хранения файлов сборки

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 call uic.cmd
+if not exist pyinstaller mkdir pyinstaller
 cd pyinstaller
 pyinstaller ../vta_video_overlay/__main__.py -n vta-video-overlay --noconfirm
 xcopy ..\LICENSE .\dist\vta-video-overlay\
