fdb5eee9c9df644a5b9278c45808a9ab65d3b796
Илья Олегович Юрьев <i.o.yurev@yandex.ru>
2024-03-18 02:06:08 +0500

    docs(linguist_fixer.py): added an explanation

--- devtools/linguist_fixer.py
+++ devtools/linguist_fixer.py
@@ -1,6 +1,9 @@
 import xml.etree.ElementTree as ET
 
 
+### For some reason pyside6-linguist becomes unresponsive when trying to select
+### a context based on a .ui file, so here's a workaround in the form of removing
+### the location information of the strings to be translated in the .ts file
 def process_xml_file(file_path: str):
     tree = ET.parse(file_path)
     contexts = tree.getroot().findall("context")
