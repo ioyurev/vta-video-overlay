290a7cd718299df9a82513e065b9663d853ff538
Илья Олегович Юрьев <i.o.yurev@yandex.ru>
2024-04-07 17:47:36 +0500

    build(uic.cmd): added check for the existence of a folder for precompilation of ui files

--- uic.cmd
+++ uic.cmd
@@ -1,3 +1,4 @@
+if not exist vta_video_overlay\ui mkdir vta_video_overlay\ui
 pyside6-uic.exe assets\MainWindow.ui > vta_video_overlay\ui\MainWindow.py
 autoflake -i --remove-all-unused-imports vta_video_overlay\ui\MainWindow.py
 pyside6-lrelease.exe .\translation_ru.ts -qm assets\translation_ru.qm
