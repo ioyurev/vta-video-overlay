347b450727753da631dc4f085527718edfcb9d02
Илья Олегович Юрьев <i.o.yurev@yandex.ru>
2024-04-19 23:06:51 +0500

    fix(uic.sh): added ui pre-compilation for crop selection window

--- uic.sh
+++ uic.sh
@@ -1,7 +1,12 @@
 if [ ! -d "vta_video_overlay/ui" ]; then
     mkdir -p vta_video_overlay/ui
 fi
+
 pyside6-uic assets/MainWindow.ui > vta_video_overlay/ui/MainWindow.py
+pyside6-uic assets/CropSelectionWindow.ui > vta_video_overlay/ui/CropSelectionWindow.py
+
 autoflake -i --remove-all-unused-imports "vta_video_overlay/ui/MainWindow.py"
+autoflake -i --remove-all-unused-imports "vta_video_overlay/ui/CropSelectionWindow.py"
+
 pyside6-lrelease "translation_ru.ts" -qm "assets/translation_ru.qm"
 pyside6-rcc "assets/resources.qrc" > "vta_video_overlay/ui/resources_rc.py"
