69aaea4e78e17e18bc42a7a055c0993ab6f11ed9
Илья Олегович Юрьев <i.o.yurev@yandex.ru>
2025-04-08 00:52:55 +0500

    build(build.cmd): added splash image generation to build script

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 call uic.cmd
 if not exist pyinstaller mkdir pyinstaller
 cd pyinstaller
-pyinstaller ../vta_video_overlay/__main__.py -n vta-video-overlay --noconfirm -i ../assets/icon.png
+python ../splash_gen.py
+pyinstaller ../vta_video_overlay/__main__.py -n vta-video-overlay --noconfirm -i ../assets/icon.png --splash splash.png
 xcopy ..\LICENSE .\dist\vta-video-overlay\
 xcopy ..\LICENSE_RU .\dist\vta-video-overlay\
