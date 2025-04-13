9dc1a5e4934a3ea9dbdf5f6ff18c82dab35c287c
Илья Олегович Юрьев <i.o.yurev@yandex.ru>
2025-04-14 01:56:00 +0500

    build: updated batch scripts

--- build.cmd
+++ build.cmd
@@ -1,7 +1,7 @@
 call uic.cmd
 if not exist pyinstaller mkdir pyinstaller
 cd pyinstaller
-python ../splash_gen.py
+python ../devtools/splash_gen.py
 pyinstaller ../vta_video_overlay/__main__.py -n vta-video-overlay --noconfirm -i ../assets/icon.png --splash splash.png
 xcopy ..\LICENSE .\dist\vta-video-overlay\
 xcopy ..\LICENSE_RU .\dist\vta-video-overlay\
--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 bash uic.sh
-if [ ! -d "pyinstaller" ]; then
-    mkdir -p pyinstaller
-fi
-cd pyinstaller
-pyinstaller ../vta_video_overlay/__main__.py -n vta-video-overlay --noconfirm -i ../assets/icon.png
-cp ../LICENSE dist/vta-video-overlay/
-cp ../LICENSE_RU dist/vta-video-overlay/
+mkdir -p pyinstaller
+cd pyinstaller || exit
+python ../devtools/splash_gen.py
+pyinstaller ../vta_video_overlay/__main__.py -n vta-video-overlay --noconfirm -i ../assets/icon.png --splash splash.png
+cp ../LICENSE ./dist/vta-video-overlay/
+cp ../LICENSE_RU ./dist/vta-video-overlay/
